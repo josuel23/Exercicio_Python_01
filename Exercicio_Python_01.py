9725a7c7a40782eb0279609206d0bcb9dcf93714
josuel23 <josuel.silva@aluno.faculdadeimpacta.com.br>
2020-04-03 18:24:49 -0300

    Create Exercicio_Python_01.py
    Conversão de graus: Fahrenheit p/ Celsius

---
+++ Exercicio_Python_01.py
@@ -0,0 +1,5 @@
+# Conversão  de Fahrenheit p/ Celsius
+
+f = float(input('Digite a temoeratura em Fahrenheit:'))
+c = (f - 32) * 5/9
+print(f'A temperatura wm graus frahrenheit: {f} convertida para graus Celcius é: {c:.2f}')
